9b4c7758a2d95da00fa07e6221fe853f4a204a65
LiamH <liamhearts@gmail.com>
2019-04-02 19:43:33 -0700

    just starting

---
+++ assets/javascript/games.js
@@ -0,0 +1,22 @@
+//what to track
+//track wins
+//track losses
+//a list of words
+//the current word to guess
+//track the wrong guess
+//track the key the user presses
+
+
+
+//what to do
+//TODO: use key events to listen for letters that players type
+//TODO: choose a word
+//TODO: restart the game
+//TODO: check if the key pressed is in the word
+//TODO: check if user won
+//TODO: check if user lost
+//TODO: display the wins
+//TODO: display the losses
+//TODO: display the wrong guesses
+//TODO: display the current word
+//TODO: display the guessed remaining
