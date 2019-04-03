b36da2db616b5991dd8bca532216e759dad91ce5
LiamH <liamhearts@gmail.com>
2019-04-02 20:01:22 -0700

    key press command working

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,6 +1,4 @@
 //what to track
-//track wins
-//track losses
 //a list of words
 //the current word to guess
 //track the wrong guess
@@ -9,7 +7,6 @@
 
 
 //what to do
-//TODO: use key events to listen for letters that players type
 //TODO: choose a word
 //TODO: restart the game
 //TODO: check if the key pressed is in the word
@@ -20,3 +17,15 @@
 //TODO: display the wrong guesses
 //TODO: display the current word
 //TODO: display the guessed remaining
+
+var wins = 0;
+var losses = 0;
+
+//TODO: use key events to listen for letters that players type
+document.onkeyup = function(event) {
+    // console.log(event.keyCode)
+    // console.log(event.key);
+    if (event.keyCode >= 65 && event.keyCode <= 90){
+        console.log(event.key)
+    }
+}
